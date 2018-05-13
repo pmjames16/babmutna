69e97d67ade20aa35808550108fdb9880eddc476
ujin43255252 <david16@kaist.ac.kr>
2018-05-14 03:53:48 +0900

    Calendar_Scroll/Highlight

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -2,40 +2,78 @@ import React, { Component } from "react";
 import "./Calendar.css";
 
 class Calendar extends Component {
-    static defaultProps = {
-        day: () => console.warn("day is not defined"),
-        recipe: () => console.warn("recipe is not defined"),
-        image: () => console.warn("image is not defined"),
-        users: () => console.warn("users are not defined")
-    };
+  static defaultProps = {
+    day: () => console.warn("day is not defined"),
+    recipe: () => console.warn("recipe is not defined"),
+    image: () => console.warn("image is not defined"),
+    users: () => console.warn("users are not defined")
+  };
 
-    render() {
-        const { date, recipe, users, selectRecipeOtherMenu,recipeIndex } = this.props;
-        const days_ = ["sunday","monday", "tuesday", "wednesday", "thursday" ,"friday","saturday", ];
-        const month = date.getMonth()+1;
-        const days = date.getDate();
-        const day = days_[date.getDay()];
-        return (
-            <div className="calendar-wrapper">
-                <div className="date">{month}/{days}</div>
-                <div className="recipe-wrapper">
-                    <img onClick={()=>selectRecipeOtherMenu(recipeIndex)} className="image" src={recipe.image} alt={recipe.name} />
-                    <div className="overlay">
-                        <div className="recipe">{recipe.name}</div>
-                        <div className="day">{day}</div>
-                    </div>
+  render() {
+    const {
+      date,
+      recipe,
+      users,
+      selectRecipeOtherMenu,
+      recipeIndex,
+      currentUser
+    } = this.props;
+    const days_ = [
+      "sunday",
+      "monday",
+      "tuesday",
+      "wednesday",
+      "thursday",
+      "friday",
+      "saturday"
+    ];
+    const month = date.getMonth() + 1;
+    const days = date.getDate();
+    const day = days_[date.getDay()];
+    return (
+      <div className="calendar-wrapper">
+        <div className="date">
+          {month}/{days}
+        </div>
+        <div className="recipe-wrapper">
+          <img
+            onClick={() => selectRecipeOtherMenu(recipeIndex)}
+            className="image"
+            src={recipe.image}
+            alt={recipe.name}
+          />
+          <div className="overlay">
+            <div className="recipe">{recipe.name}</div>
+            <div className="day">{day}</div>
+          </div>
+        </div>
+        <div className="users-wrapper">
+          {users.map(
+            user =>
+              user.id === currentUser.id ? (
+                <div className="user-wrapper" key={user.name}>
+                  <img
+                    className="user-image"
+                    src={user.image}
+                    alt={user.name}
+                  />
+                  <div className="user-name currentuser">{user.name}</div>
                 </div>
-                <div className="users-wrapper">
-                    {users.map(user => (
-                        <div className="user-wrapper" key={user.name}>
-                            <img className="user-image" src={user.image} alt={user.name} />
-                            <div className="user-name">{user.name}</div>
-                        </div>
-                    ))}
+              ) : (
+                <div className="user-wrapper" key={user.name}>
+                  <img
+                    className="user-image"
+                    src={user.image}
+                    alt={user.name}
+                  />
+                  <div className="user-name">{user.name}</div>
                 </div>
-            </div>
-        );
-    }
+              )
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Calendar;
--- src/components/Calendar/CalendarTemplate.js
+++ src/components/Calendar/CalendarTemplate.js
@@ -1,47 +1,106 @@
 import React, { Component } from "react";
 import "./CalendarTemplate.css";
-import Calendar from './Calendar'
+import Calendar from "./Calendar";
+let current_pos = 0;
 
 class CalendarTemplate extends Component {
-
-    calendarMapping = () => {
-        const { users, recipes, selectRecipeOtherMenu } = this.props;
-        const currentDate = new Date();
-        let weeks = [];
-        for(let i = 0;i<7;i++){
-            let day = new Date();
-            day.setDate(currentDate.getDate()+i);
-            weeks.push(day);
-        }
-        const calendar_list = weeks.map((day,index)=>{
-            let dayUser = users.slice(((index*3)%users.length),((index*3)%users.length+3));
-            let count = 0;
-            while(dayUser.length < 3){
-                dayUser.push(users[count]);
-                count++;
-            };
-            return (
-                <Calendar
-                    date={day}
-                    recipe={recipes[index%(recipes.length)]}
-                    users={dayUser}
-                    key={index}
-                    recipeIndex = {index%(recipes.length)}
-                    selectRecipeOtherMenu = {selectRecipeOtherMenu}
-                />
-            );
-        });
-        return calendar_list;
-    };
-
-    render() {
-        return (
-            <div className="calendar-template">
-                {this.calendarMapping()}
-            </div>
-        );
+  state = {
+    user_selected: -1,
+    visited: -1,
+    current_position: 0
+  };
+  componentDidMount() {
+    const { currentUser } = this.props;
+    const { current_position } = this.state;
+    if (currentUser.id !== -1) {
+      //   window.scrollTo(0, 54 + 174 * current_pos);
+      window.scrollTo({
+        top: 44 + 184 * current_pos,
+        left: 0,
+        behavior: "smooth"
+      });
+      this.setState({
+        user_selected: currentUser.id
+      });
     }
+  }
+  componentDidUpdate(nextProps, nextState) {
+    const { currentUser } = this.props;
+    const { current_position } = this.state;
+    if (nextProps.currentUser.id !== currentUser.id && currentUser.id !== -1) {
+      //   window.scrollTo(0, 44 + 184 * current_pos);
+      window.scrollTo({
+        top: 44 + 184 * current_pos,
+        left: 0,
+        behavior: "smooth"
+      });
+      this.setState({
+        user_selected: currentUser.id
+      });
+    }
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextProps.currentUser.id !== this.props.currentUser.id) {
+      this.setState({
+        visited: -1
+      });
+      return true;
+    }
+    if (this.state.visited === -1) {
+      this.setState({
+        visited: 1
+      });
+      return true;
+    } else {
+      return false;
+    }
+  }
+  componentWillUnmount = () => {
+    this.setState({
+      visited: -1
+    });
+  };
+  calendarMapping = () => {
+    const { users, recipes, selectRecipeOtherMenu, currentUser } = this.props;
+    const currentDate = new Date();
+    let weeks = [];
+    for (let i = 0; i < 7; i++) {
+      let day = new Date();
+      day.setDate(currentDate.getDate() + i);
+      weeks.push(day);
+    }
+    let bull = 0;
+    const calendar_list = weeks.map((day, index) => {
+      let dayUser = users.slice(
+        (index * 3) % users.length,
+        (index * 3) % users.length + 3
+      );
+      let count = 0;
+      while (dayUser.length < 3) {
+        dayUser.push(users[count]);
+        count++;
+      }
+      dayUser.map(user => user.id === currentUser.id && (current_pos = bull));
+      bull++;
+      return (
+        <Calendar
+          date={day}
+          recipe={recipes[index % recipes.length]}
+          users={dayUser}
+          key={index}
+          recipeIndex={index % recipes.length}
+          selectRecipeOtherMenu={selectRecipeOtherMenu}
+          currentUser={currentUser}
+        />
+      );
+    });
+    return calendar_list;
+  };
+
+  render() {
+    console.log(this.props.users[3].id, current_pos, this.state.user_selected);
+    return <div className="calendar-template">{this.calendarMapping()}</div>;
+  }
 }
 
 export default CalendarTemplate;
-
